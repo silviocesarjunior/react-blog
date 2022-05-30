e742ca7be995cc88635c88b70de81f64d110e383
silviocesarjunior <silvio_czar_cpv@hotmail.com>
2022-05-30 07:08:08 -0300

    pages

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import "./home.css"
+import Header from "../../header/Header"
+import Posts from "../../posts/Posts";
+import Sidebar from "../../sidebar/Sidebar";
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <div className="home">
+                <Posts />
+                <Sidebar />
+            </div>
+        </>
+    );
+}
---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,37 @@
+import "./sidebar.css"
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg" alt="" />
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Sapiente cupiditate distinctio rem, expedita sunt eius quidem possimus,
+                    consequatur quia dolorem suscipit magni at!
+                </p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fa-brands fa-facebook"></i>
+                    <i className="sidebarIcon fa-brands fa-twitter"></i>
+                    <i className="sidebarIcon fa-brands fa-pinterest"></i>
+                    <i className="sidebarIcon fa-brands fa-instagram"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
