64ea147634ca861fde006f7a81f80e554f5d4660
silviocesarjunior <silvio_czar_cpv@hotmail.com>
2022-06-01 07:14:31 -0300

    request

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react";
 import "./home.css"
 import Header from "../../components/header/Header"
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
+import axios from "axios";
 
 export default function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get("/posts");
+            setPosts(res.data)
+        };
+        fetchPosts();
+    }, []);
     return (
         <>
             <Header />
             <div className="home">
-                <Posts />
+                <Posts posts={posts} />
                 <Sidebar />
             </div>
         </>
